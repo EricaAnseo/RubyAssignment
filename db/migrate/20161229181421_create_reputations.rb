acd0f5c6d63f249740ff960bac4a0b9a67e749f3
Erica Chai <Erica Chai>
2017-01-02 23:40:34 +0000

    DB Fixes

--- db/migrate/20161229181421_create_reputations.rb
+++ db/migrate/20161229181421_create_reputations.rb
@@ -1,8 +1,8 @@
 class CreateReputations < ActiveRecord::Migration[5.0]
   def change
     create_table :reputations do |t|
-      t.references :reviewer, foreign_key: true, references: :users
-      t.references :reviewee, foreign_key: true, references: :users
+      t.belongs_to :reviewer, foreign_key: true, references: :users
+      t.belongs_to :reviewee, foreign_key: true, references: :users
       t.integer :rating
       t.string :comment
 
